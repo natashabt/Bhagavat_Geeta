3da609278467a5479b536776065cf51f501a913e
natashabt <149521671+natashabt@users.noreply.github.com>
2025-04-02 20:29:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Bhagavat_Geeta
-Developed a responsive educational website using HTML and Tailwind CSS, featuring animated navigation and marquee text, to address frequently asked questions about the Bhagavad Gita.
+# The_Song_of_god
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('6th.jpg')",
+        'footer-texture': "url('10th.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
